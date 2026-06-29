5db69a71e4c976321f996375e7c17cf9b0cf00ef
jozhua <joshuazabala693@gmail.com>
2026-06-29 23:06:22 +0800

    refactored renderPage names and fixed the admin object to get data from session. Added CRUD controle for Rooms Page

--- controllers/AdminController.js
+++ controllers/AdminController.js
@@ -2,15 +2,24 @@ const pool = require('../configs/db');
 const b = require('bcrypt');
 const UserModel = require('../models/UserModel');
 const DepartmentModel = require('../models/DepartmentModel');
+const RoomModel = require('../models/RoomModel');
 
 const AdminController = {
-    
-    async renderPage(req,res) {
+
+    // USERS
+    async renderUsersPage(req, res) {
         const admin = {
-            // id: req.user?.id || req.session?.id || 1,
-            id: 10,
-            name: req.user?.name || req.session?.name || 'Admin CCS',
-            role: req.user?.role || req.session?.role || 'admin',
+            id: req.session?.instructorId,
+            name: req.session?.name,
+            firstName: req.session.firstName,
+            middleName: req.session.middleName,
+            lastName: req.session.lastName,
+            status: req.session.status,
+            email: req.session?.email,
+            position: req.session.position,
+            role: req.session.role,
+            profilePhoto: req.session?.profilePhoto || 'N/A',
+            department: req.session?.department,
         }
 
         const [users, departments] = await Promise.all([
@@ -28,9 +37,9 @@ const AdminController = {
 
     async createUser(req, res) {
         try {
-            const { firstName, middleName, lastName, role, 
-                status, email, departmentId, password, 
-                employmentType, position, profilePicture} = req.body;
+            const { firstName, middleName, lastName, role,
+                status, email, departmentId, password,
+                employmentType, position, profilePicture } = req.body;
 
             const errors = {};
 
@@ -73,9 +82,9 @@ const AdminController = {
             })
         } catch (err) {
             if (err.code === 'ER_DUP_ENTRY') {
-                return res.status(409).json({ 
-                    success: false, 
-                    errors: { 
+                return res.status(409).json({
+                    success: false,
+                    errors: {
                         email: 'Email already exists.'
                     }
                 });
@@ -94,11 +103,11 @@ const AdminController = {
             const { firstName, middleName, lastName, email, role, departmentId, status, employmentType, position } = req.body;
 
             const errors = {};
-            if (!publicId)               errors.id             = `User's identifier is missing.`;
-            if (!firstName?.trim())      errors.firstName      = 'First name is required.';
-            if (!lastName?.trim())       errors.lastName       = 'Last name is required.';
-            if (!email?.trim())          errors.email          = 'Email is required.';
-            if (!role?.trim())           errors.role           = 'Role is required.';
+            if (!publicId) errors.id = `User's identifier is missing.`;
+            if (!firstName?.trim()) errors.firstName = 'First name is required.';
+            if (!lastName?.trim()) errors.lastName = 'Last name is required.';
+            if (!email?.trim()) errors.email = 'Email is required.';
+            if (!role?.trim()) errors.role = 'Role is required.';
             if (!employmentType?.trim()) errors.employmentType = 'Employment type is required.';
             if (!position?.trim()) errors.position = 'Position/Title is required.';
 
@@ -139,6 +148,145 @@ const AdminController = {
         } catch (err) {
             console.error('[AdminController.deleteUser]', err);
             res.status(500).json({ success: false, error: 'Failed to delete user.' });
+        }
+    },
+
+    // ROOMS 
+    async renderRoomsPage(req, res) {
+        const admin = {
+            id: req.session?.instructorId,
+            name: req.session?.name,
+            firstName: req.session.firstName,
+            middleName: req.session.middleName,
+            lastName: req.session.lastName,
+            status: req.session.status,
+            email: req.session?.email,
+            position: req.session.position,
+            role: req.session.role,
+            profilePhoto: req.session?.profilePhoto || 'N/A',
+            department: req.session?.department,
+        };
+
+        const [departments, rooms] = await Promise.all([
+            DepartmentModel.getDepartments(),
+            RoomModel.getRooms({
+                fields: `r.id, 
+                        r.room_number, 
+                        r.room_type, 
+                        r.department_id,
+                        d.full_name AS department_name, 
+                        d.building AS building_name, 
+                        CONCAT(u.last_name + ', ' + u.first_name) AS assigned_faculty_name, 
+                        r.is_ble_scanner_installed, 
+                        r.status`
+            })
+        ]);
+
+        res.render('pages/admin/rooms', {
+            title: 'FaciTrack - Rooms Management',
+            admin: admin,
+            departments: departments,
+            roomData: rooms
+        });
+    },
+
+    async createRoom(req, res) {
+        try {
+            const { roomNumber, department, roomType, bleStatus, assignedFaculty, status } = req.body;
+
+            const errors = {};
+
+            if (!roomNumber) errors.roomNumber = 'Room Number is required.';
+            if (!department) errors.department = 'Department is required.';
+            if (!roomType) errors.roomType = 'Room Type is required.';
+            if (!bleStatus) errors.bleStatus = 'BLE Scanner status is required.';
+            if (!status) errors.status = 'Status is required.';
+
+            // return early if at least one error is present
+            if (Object.keys(errors).length > 0) {
+                return res.status(422).json({ success: false, errors })
+            }
+
+            // await for the room model to finish inserting new room
+            await RoomModel.insertRoomByAdmin({
+                roomNumber,
+                department,
+                roomType,
+                bleStatus,
+                assignedFaculty,
+                status
+            });
+
+            return res.status(200).json({
+                success: true,
+                message: 'Room created successfully!'
+            })
+        } catch (err) {
+            console.error(`[AdminController.createRoom] ${err}`);
+            res.status(500).json({
+                success: false,
+                error: 'Failed to create room.'
+            });
+        }
+    },
+
+    async updateRoom(req, res) {
+        try {
+            const { roomId } = req.params;
+            const { roomNumber, department, roomType, bleStatus, assignedFaculty, status } = req.body;
+
+            const errors = {};
+
+            if (!roomNumber) errors.roomNumber = 'Room Number is required.';
+            if (!department) errors.department = 'Department is required.';
+            if (!roomType) errors.roomType = 'Room Type is required.';
+            if (!status) errors.status = 'Status is required.';
+
+            // return early if at least one error is present
+            if (Object.keys(errors).length > 0) {
+                return res.status(422).json({ success: false, errors })
+            }
+
+            // await for the room model to finish updating new room
+            await RoomModel.updateRoom(roomId, {
+                roomNumber,
+                department,
+                roomType,
+                bleStatus,
+                assignedFaculty: assignedFaculty || null,
+                status
+            });
+
+            return res.status(200).json({
+                success: true,
+                message: 'Room updated successfully!'
+            })
+        } catch (err) {
+            console.error(`[AdminController.updateRoom] ${err}`);
+            res.status(500).json({
+                success: false,
+                error: 'Failed to create room.'
+            });
+        }
+    },
+
+    async deleteRoom(req, res) {
+        try {
+            const { roomId } = req.params;
+
+            if (!roomId) errors.id = `Room's identifier is missing.`;
+
+            const affectedRows = await RoomModel.deleteRoom(roomId);
+
+            if (affectedRows === 0) {
+                return res.status(404).json({ success: false, error: 'Room not found.' });
+            }
+
+            res.json({ success: true, message: `Room deleted successfully!` });
+
+        } catch (err) {
+            console.error('[AdminController.deleteRoom]', err);
+            res.status(500).json({ success: false, error: 'Failed to delete room.' });
         }
     }
 }
