286b33bf569685e291636fbcf7c60d812547d8b5
jozhua <joshuazabala693@gmail.com>
2026-08-10 22:03:51 +0800

    added buildInstructorUser

--- utils/sessionUser.js
+++ utils/sessionUser.js
@@ -12,6 +12,23 @@ function buildStudentUser(session, extraFields = {}) {
     }
 }
 
+function buildInstructorUser(session) {
+    return {
+        id: session?.userId,
+        name: session?.name,
+        firstName: session.firstName,
+        middleName: session.middleName,
+        lastName: session.lastName,
+        status: session.status,
+        email: session?.email,
+        position: session.position,
+        role: session.role,
+        profilePhoto: session?.profilePhoto || null,
+        department: session?.department,
+    }
+}
+
 module.exports = {
-    buildStudentUser
+    buildStudentUser,
+    buildInstructorUser,
 };
